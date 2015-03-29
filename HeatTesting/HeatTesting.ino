41bac42ef7d77636ee262134c7d83ec1b11d5095
Jazel Canseco <jcanseco@uwaterloo.ca>
2015-03-29 07:14:19 -0400

    implemented RGB LED

--- HeatTesting/HeatTesting.ino
+++ HeatTesting/HeatTesting.ino
@@ -1,16 +1,71 @@
-int potPin = 2;
-int ledPin = 13;
-int val = 0; // variable to store the value coming from the sensors
+int prevR = 0, prevG = 0, prevB = 0; // all of the previous RGB values
+int const red = 3; 
+int const blue = 5; 
+int const green = 6; 
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
+void setup()
+{
+} 
 
+void loop() { 
+  RGB(255, 255, 255); // calls the RGB function
+  delay(1000); //stays on white for one second
+  RGB(0, 0, 255); 
+   delay(1000);
+   RGB(0,120,255);
+  delay(1000);
+    RGB(0, 255, 0);
+  delay(1000);
+    RGB(255, 0, 255);
+  delay(10);
+    RGB(0,0,0);
+  delay(1000);
+ 
 }
 
-void loop() {
-  val = analogRead(potPin);
-  digitalWrite(ledPin, HIGH);
-  delay(val);
-  digitalWrite(ledPin, LOW);
-  delay(val);
+
+void RGB(int R, int G, int B) {
+for (int i = 0; i <= 255; i++)
+{
+if (i >=  prevR - R && prevR < R) {
+ 
+  analogWrite(Red, prevR + i);
+}
+if (i >= prevG - G && prevG < G) {
+ 
+  analogWrite(Green, prevG + i);
+ 
+}
+if (i >= prevB - B && prevB < B) {
+ 
+  analogWrite(Blue, prevB + i);
+ 
+}
+//delay(10);
+//}
+//for (int i = 0; i <= 255; i++)
+//{
+if (i >= R - prevR && prevR > R) {
+ 
+  analogWrite(Red, prevR - i);
+}
+if (i >= G - prevG && prevG > G) {
+ 
+  analogWrite(Green, prevG - i);
+ 
+}
+if (i >= B - prevB && prevB > B) {
+ 
+  analogWrite(Blue, prevB - i);
+ 
+}
+delay(10);
+}
+delay(10);
+analogWrite(Red, R);
+analogWrite(Green, G);
+analogWrite(Blue, B);
+prevR = R;
+prevG = G;
+prevB = B;
 }
